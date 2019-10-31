086b134bd4b3cf513a26b490c918c7c3db8d376f
z-apollo <54058143+z-apollo@users.noreply.github.com>
2019-10-31 15:09:11 +0800

    7.跳转到商品列表页获取路径参数

--- pages/goods_list/index.js
+++ pages/goods_list/index.js
@@ -1,4 +1,16 @@
 Page({
+  data: {
+    query: ""
+  },
 
- 
+  onLoad: function(options){
+    // options是页面传的参数，比如 {query: "曲面电视"}
+    const {query} = options
+
+    this.setData({
+      // query:query
+      query
+    })
+  }
+
 })
