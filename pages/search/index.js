f65f883f2f184c4f3f53d118e42d94e30f80c440
z-apollo <54058143+z-apollo@users.noreply.github.com>
2019-11-01 16:05:47 +0800

    18.点击取消按钮清空输入框的值

--- pages/search/index.js
+++ pages/search/index.js
@@ -1,22 +1,31 @@
 // pages/search/index.js
 Page({
   data: {
-    showCancel:false
+    showCancel:false,
+    searchValue:""
   },
-  
+
   onLoad: function (options) {
 
   },
 
   handleInput(event){
-    console.log(event)
+    // console.log(event)
     //输入框的值
     const {value} = event.detail
     let showCancel
     //判断输入框有没有值，value.trim()去除前后空格
     showCancel = value.trim() ? true : false
     this.setData({
-      showCancel:showCancel
+      showCancel:showCancel,
+      searchValue:value
+    })
+  },
+
+  handleCancel(){
+    this.setData({
+      searchValue:"",
+      showCancel:false
     })
   }
 
