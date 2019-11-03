d595415f4a151eca67aa8a84e264c22c0b3eb0af
z-apollo <54058143+z-apollo@users.noreply.github.com>
2019-11-03 16:13:13 +0800

    23.获取收货地址

--- pages/cart/index.js
+++ pages/cart/index.js
@@ -2,8 +2,24 @@
 Page({
 
   data: {
+    //收货地址
+    address:{}
+  },
 
+  //获取收货地址
+  handleAddress(){
+    wx.chooseAddress({
+      //成功的方法
+      success:(res) =>{
+        //设置收货地址
+        this.setData({
+          address:{
+            userName: res.userName,
+            telNumber: res.telNumber,
+            detail: res.provinceName + res.cityName + res.countyName + res.detailInfo
+          }
+        })
+      }
+    })
   }
-
-
 })
